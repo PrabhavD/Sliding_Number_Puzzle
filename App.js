512e34801879e6ab5f7f1f783ca057067f0f4afd
Prabhav Desai <desaipd1220@gmail.com>
2021-04-28 17:40:23 -0600

    Create layout

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
 // Year Zero Studios - Week 0 - Sliding Number Puzzle
 // By: Prabhav Desai
 
+class Puzzle extends React.Component {
+    render() {
+        return (
+            <Tiles rows={3} cols={3} hole={8} width={500} height={500}/>
+
+        )
+    }
+}
+
