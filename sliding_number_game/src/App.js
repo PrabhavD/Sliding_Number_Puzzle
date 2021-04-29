f4d959285a7233a2628d3918df5d59185bd9a539
Prabhav Desai <desaipd1220@gmail.com>
2021-04-28 18:30:18 -0600

    Save Progress

--- sliding_number_game/src/App.js
+++ sliding_number_game/src/App.js
@@ -2,6 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var Board = React.createClass({
+
+
+    getInitialState: function() {
+      return {
+        tiles: this.shuffle([1, 2, 3, 4, 5, 6, 7, 8, '']),
+        complete: false
+      };
+    },
+
+    checkBoard: function() {
+      var tiles = this.state.tiles;
+
+      for (var i = 0; i< tiles.length-1; i++) {
+        if (tiles[i] !== i+1) return false;
+        else return true;
+      }
+
+    }
+  })
+
   return (
     <div className="App">
       <header className="App-header">
